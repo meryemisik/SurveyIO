c3a6994eb6405198efd99ff4d46433fbee3422b7
meryem <mmeryemisik@gmail.com>
2023-02-13 23:48:10 +0300

    fixede

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,9 @@ import VueSocketIO from 'vue-socket.io';
 Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
-  connection: 'https://mat-vote-app.onrender.com'
+  connection: 'https://mat-vote-app.onrender.com',
+  transports: ['websocket', 'polling', 'flashsocket']
+
 }));
 new Vue({
   render: h => h(App),
