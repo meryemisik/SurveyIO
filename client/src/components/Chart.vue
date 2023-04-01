d1136cfdd96070181394dc4917270d4772de2df8
meryem <mmeryemisik@gmail.com>
2023-04-02 00:42:38 +0300

    UPDATE: project update

--- client/src/components/Chart.vue
+++ client/src/components/Chart.vue
@@ -1,12 +1,57 @@
 <template>
-  <div class="chart">
-    <canvas ref="canvas" id="canvas"></canvas>
-    <div class="chart-vote-button">
-      <button @click="addVote('a')" :disabled="isButtonDisabled">A</button>
-      <button @click="addVote('b')" :disabled="isButtonDisabled">B</button>
-      <button @click="addVote('c')" :disabled="isButtonDisabled">C</button>
-      <button @click="addVote('d')" :disabled="isButtonDisabled">D</button>
+  <div>
+    <div>
+      <Sidebar :visible.sync="visibleLeft" class="sidebar-list">
+        <div class="sidebar-list-item">
+          Liste
+          <Button
+            icon="pi pi-trash"
+            class="p-button-text sidebar-list-item-delete"
+          />
+        </div>
+        <div class="sidebar-list-item">
+          Liste
+          <Button
+            icon="pi pi-trash"
+            class="p-button-text sidebar-list-item-delete"
+          />
+        </div>
+        <Button
+          label="Add Vote"
+          class="p-button-success sidebar-list-add-vote"
+          @click="addVoteFunction()"
+        />
+      </Sidebar>
+      <Button @click="visibleLeft = true" label="Submit" />
     </div>
+    <div class="chart">
+      <canvas ref="canvas" id="canvas"></canvas>
+      <div class="chart-vote-button">
+        <button @click="addVote('a')" :disabled="isButtonDisabled">A</button>
+        <button @click="addVote('b')" :disabled="isButtonDisabled">B</button>
+        <button @click="addVote('c')" :disabled="isButtonDisabled">C</button>
+        <button @click="addVote('d')" :disabled="isButtonDisabled">D</button>
+      </div>
+    </div>
+
+    <Dialog
+      header="Add Vote"
+      :visible.sync="addVoteModalVisible"
+      class="sidebar-list-add-vote-modal"
+    >
+      <Dropdown
+        v-model="selectedChart"
+        :options="charts"
+        optionLabel="name"
+        placeholder="Select Chart Type"
+        @change="changeChartType($event)"
+      />
+
+      <div class="chart">
+        --- {{ this.newChart[0].chartType }}
+        <canvas class="new-chart"></canvas>
+      </div>
+    </Dialog>
   </div>
 </template>
 
@@ -19,8 +64,8 @@ export default {
       console.log("socket connected 2");
     },
     dataSendFront(data) {
-      if (this.chartBar != null) {
-        this.chartBar.destroy();
+      if (this.chartBarx != null) {
+        this.chartBarx.destroy();
       }
       this.dataSendChild = data;
       this.chart();
@@ -28,15 +73,41 @@ export default {
   },
   data() {
     return {
+      newChart:[
+        {
+          chartType:'bubble',
+          chartTitle : '',
+          chartColumns:[
+            {
+              label:'',
+              bgColor:'',
+              borderColor:''
+            }
+          ]
+
+        }
+      ],
+      addVoteModalVisible: false,
+      visibleLeft: false,
       dataSendChild: null,
       chartBar: null,
+      chartBarx:null,
       isButtonDisabled: false,
+      selectedChart: null,
+      charts: [
+        { name: "Bar Chart", type: "bar" },
+        { name: "Bubble Chart", type: "bubble" },
+        { name: "Doughnut and Pie Charts", type: "doughnut" },
+        { name: "Line Chart", type: "line" },
+        { name: "Polar Area Chart", type: "polarArea" },
+        { name: "Radar Chart", type: "radar" },
+        { name: "Scatter Chart", codtypee: "scatter" },
+      ],
     };
   },
 
   methods: {
     chart() {
-      console.log("obje value", Object.values(this.dataSendChild));
       var ctx = document.getElementById("canvas").getContext("2d");
       this.chartBar = new Chart(ctx, {
         type: "bar",
@@ -88,10 +159,65 @@ export default {
       this.$socket.emit("voteSendServer", e);
       this.isButtonDisabled = true;
     },
+    addVoteFunction() {
+      this.visibleLeft = false;
+      this.addVoteModalVisible = true;
+    },
+    changeChartType(e) {
+      console.log("e", e.value.type);
+      this.newChart[0].chartType = e.value.type
+
+      var ctx = document.getElementsByClassName('new-chart').getContext("2d");
+      console.log(this.newChart[0].chartType)
+      this.chartBarx = new Chart(ctx, {
+        type:this.newChart[0].chartType,
+        data: {
+          labels: ["Label One", "Label Two"],
+          datasets: [
+            {
+              label: "Your Vote Title",
+              data: Object.values(this.dataSendChild),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)"
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          onClick: (e) => {
+            var points = e.chart.getElementsAtEventForMode(
+              e,
+              "nearest",
+              { intersect: true },
+              true
+            );
+            if (points.length) {
+              const firstPoint = points[0];
+              var label = e.chart.data.labels[firstPoint.index];
+            }
+            this.addVote(label);
+          },
+        },
+      });
+
+
+    },
   },
 };
 </script>
 
 <style lang="scss">
-@import '../assets/scss/chartBar.scss';
+@import "../assets/scss/chartBar.scss";
+@import "../assets/scss/sidebar.scss";
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSocketIO from 'vue-socket.io';
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'          
+import 'primeicons/primeicons.css'                        
 
 
+//component
+import Sidebar from 'primevue/sidebar';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import Dropdown from 'primevue/dropdown';
+
+Vue.use(PrimeVue);
 Vue.config.productionTip = false
+
+Vue.component('Sidebar', Sidebar);
+Vue.component('Button', Button);
+Vue.component('Dialog', Dialog);
+Vue.component('Dropdown', Dropdown);
 
 Vue.use(new VueSocketIO({
   debug: true,
