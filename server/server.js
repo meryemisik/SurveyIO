42edbc14a188872ca314c1e2c3cbeaeefaf90086
meryem <mmeryemisik@gmail.com>
2023-05-17 19:02:10 +0300

    START: firebase connection

--- server/server.js
+++ server/server.js
@@ -1,3 +1,43 @@
+// const { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } = require("./firebase-config/index");
+//import { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } from "./firebase-config/index.js";
+
+const { initializeApp, firebase } = require("firebase/app");
+const { getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  updateEmail,
+  updateProfile,
+  signOut
+} = require("firebase/auth");
+const { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } = require("firebase/firestore") ;
+// import { initializeApp } from "firebase/app";
+// import { getAuth,
+//   RecaptchaVerifier,
+//   signInWithPhoneNumber,
+//   updateEmail,
+//   updateProfile,
+//   signOut
+// } from "firebase/auth";
+// import { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyCsRWt3e5ZsgaLDybCAolXgR8rRFrTxV_M",
+    authDomain: "mat-vote-app.firebaseapp.com",
+    projectId: "mat-vote-app",
+    storageBucket: "mat-vote-app.appspot.com",
+    messagingSenderId: "147812523519",
+    appId: "1:147812523519:web:6587e52f05afe4234bdbca",
+    measurementId: "G-0HMKP9GRYL"
+  };
+const appx = initializeApp(firebaseConfig);
+const auth = getAuth(appx);
+const db = getFirestore(app);
+
+auth.languageCode = "tr";
+
+// module.exports = {auth: function(){}, db: function(){}, RecaptchaVerifier: function(){}, signInWithPhoneNumber: function(){}, 
+// updateEmail: function(){}, updateProfile: function(){}, signOut: function(){}, getFirestore: function(){}, collection: function(){}, 
+// getDocs: function(){}, doc: function(){}, setDoc: function(){}, query: function(){}, orderBy: function(){}}
+ 
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http, {
@@ -104,9 +144,16 @@ var userVote = [
 app.get('/', function (req, res) {
     res.send()
 })
+const test = async() => {
+    const querySnapshot = await getDocs(collection(db, "user"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+}
 //siteye giriş yapan kullanıcının bilgisini alır
 //işlemler ve dinlemeler bunun içinde gerçekleşir
 io.on('connection', function (socket) {
+    test()
     //io.emit tüm tarayıcılara gider
     //socket.emit sadece benim tarayıcıma gelir
     //socket.broadcast benim dışımdaki diğer tarayıcılara gider
@@ -117,7 +164,7 @@ io.on('connection', function (socket) {
                 Math.random() * Math.pow(10, 20),
             )}-${new Date().getTime()}`, createdDate: new Date()
         })
-        io.emit('dataSendFront', { surveyList: surveyList, userVote: userVote , userPhone: '05372997045'})
+        io.emit('dataSendFront', { surveyList: surveyList, userVote: userVote, userPhone: '05372997045' })
     })
     socket.on('voteSendServer', function (e) {
         if (!!e.label && !!e.id) {
