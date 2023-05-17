b541433283419bca160acb7e31492e1830111525
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2023-05-17 20:14:52 +0300

    feat: firebase modules export/import done

--- server/firebase-config/index.js
+++ server/firebase-config/index.js
@@ -1,7 +1,3 @@
-// const { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } = require("./firebase-config/index");
-//import { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } from "./firebase-config/index.js";
-
-
 const { initializeApp, firebase } = require("firebase/app");
 const { getAuth,
   RecaptchaVerifier,
@@ -10,33 +6,24 @@ const { getAuth,
   updateProfile,
   signOut
 } = require("firebase/auth");
-const { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } = require("firebase/firestore") ;
-// import { initializeApp } from "firebase/app";
-// import { getAuth,
-//   RecaptchaVerifier,
-//   signInWithPhoneNumber,
-//   updateEmail,
-//   updateProfile,
-//   signOut
-// } from "firebase/auth";
-// import { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore";
+const { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } = require("firebase/firestore");
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCsRWt3e5ZsgaLDybCAolXgR8rRFrTxV_M",
-    authDomain: "mat-vote-app.firebaseapp.com",
-    projectId: "mat-vote-app",
-    storageBucket: "mat-vote-app.appspot.com",
-    messagingSenderId: "147812523519",
-    appId: "1:147812523519:web:6587e52f05afe4234bdbca",
-    measurementId: "G-0HMKP9GRYL"
-  };
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCsRWt3e5ZsgaLDybCAolXgR8rRFrTxV_M",
+  authDomain: "mat-vote-app.firebaseapp.com",
+  projectId: "mat-vote-app",
+  storageBucket: "mat-vote-app.appspot.com",
+  messagingSenderId: "147812523519",
+  appId: "1:147812523519:web:6587e52f05afe4234bdbca",
+  measurementId: "G-0HMKP9GRYL"
+};
 const appx = initializeApp(firebaseConfig);
 const auth = getAuth(appx);
-const db = firebase.firestore();
+const db = getFirestore(appx);
 
 auth.languageCode = "tr";
 
-module.exports = {auth: function(){}, db: function(){}, RecaptchaVerifier: function(){}, signInWithPhoneNumber: function(){}, 
-updateEmail: function(){}, updateProfile: function(){}, signOut: function(){}, getFirestore: function(){}, collection: function(){}, 
-getDocs: function(){}, doc: function(){}, setDoc: function(){}, query: function(){}, orderBy: function(){}}
+module.exports = {auth, db, RecaptchaVerifier, signInWithPhoneNumber, 
+updateEmail, updateProfile, signOut, getFirestore, collection, 
+getDocs, doc, setDoc, query, orderBy}
  
--- server/server.js
+++ server/server.js
@@ -1,43 +1,3 @@
-// const { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } = require("./firebase-config/index");
-//import { auth, db, RecaptchaVerifier, signInWithPhoneNumber, updateEmail, updateProfile, signOut, getFirestore, collection, getDocs, doc, setDoc, query, orderBy } from "./firebase-config/index.js";
-
-const { initializeApp, firebase } = require("firebase/app");
-const { getAuth,
-  RecaptchaVerifier,
-  signInWithPhoneNumber,
-  updateEmail,
-  updateProfile,
-  signOut
-} = require("firebase/auth");
-const { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } = require("firebase/firestore") ;
-// import { initializeApp } from "firebase/app";
-// import { getAuth,
-//   RecaptchaVerifier,
-//   signInWithPhoneNumber,
-//   updateEmail,
-//   updateProfile,
-//   signOut
-// } from "firebase/auth";
-// import { doc, setDoc, getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore";
-const firebaseConfig = {
-    apiKey: "AIzaSyCsRWt3e5ZsgaLDybCAolXgR8rRFrTxV_M",
-    authDomain: "mat-vote-app.firebaseapp.com",
-    projectId: "mat-vote-app",
-    storageBucket: "mat-vote-app.appspot.com",
-    messagingSenderId: "147812523519",
-    appId: "1:147812523519:web:6587e52f05afe4234bdbca",
-    measurementId: "G-0HMKP9GRYL"
-  };
-const appx = initializeApp(firebaseConfig);
-const auth = getAuth(appx);
-const db = getFirestore(app);
-
-auth.languageCode = "tr";
-
-// module.exports = {auth: function(){}, db: function(){}, RecaptchaVerifier: function(){}, signInWithPhoneNumber: function(){}, 
-// updateEmail: function(){}, updateProfile: function(){}, signOut: function(){}, getFirestore: function(){}, collection: function(){}, 
-// getDocs: function(){}, doc: function(){}, setDoc: function(){}, query: function(){}, orderBy: function(){}}
- 
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http, {
@@ -47,6 +7,9 @@ var io = require('socket.io')(http, {
 })
 var PORT = process.env.PORT || 3002
 
+const { auth, db, RecaptchaVerifier, signInWithPhoneNumber,
+    updateEmail, updateProfile, signOut, getFirestore, collection,
+    getDocs, doc, setDoc, query, orderBy } = require("./firebase-config");
 
 var surveyList = [
     {
@@ -144,10 +107,10 @@ var userVote = [
 app.get('/', function (req, res) {
     res.send()
 })
-const test = async() => {
+const test = async () => {
     const querySnapshot = await getDocs(collection(db, "user"));
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
+        console.log(`${doc.id} => ${doc.data()}`);
     });
 }
 //siteye giriş yapan kullanıcının bilgisini alır
